efa8c4d5af4d948c72405b70a61f5cf2c6ab317c
Marwa-arsal <marwa.arsal@hotmail.com>
2024-12-20 13:07:32 -0800

    js.work

---
+++ topic1/apps.js
@@ -0,0 +1,50 @@
+// chapter # 01
+alert ("welcome here");
+// q#2
+alert("Error! Please enter a valid password.");
+// q#3
+alert("Welcome to JS land....\n Happing coding!");
+// q#4
+alert("Welcome to JS land...");
+alert("Happing coding! \n Prevent this page from creating additional dilogs.");
+alert("Hello...I can run JS through my web browser'console")
+var answer = prompt("What does HTML stand for?");
+if (answer === "hyper text markup language") {
+    alert("correct") ;
+}
+//chapter 02
+
+// question # 01
+var username;
+// question # 02
+var myName = "marwa arsal";
+// question # 03
+var message;
+message = "Hello World"
+alert(message);
+// question # 04 (1)
+var data = "Jhone Doe"
+alert(data);
+// question # 04 (2)
+var age = "15 years old"
+alert(age);
+// question # 04 (3)
+var add = "certified mobile application development"
+alert(add);
+// question # 05
+var lines = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(lines);
+// question # 06
+var email = "marwa.arsal@gmail.com";
+var address = " My email address is "
+alert(address + email)
+// question # 07
+var book= " A smarter way to learn JavaScript"
+var way= "I am trying to learn from the book  javascript"
+alert(way+book);
+// question # 08
+var work = "Yah! i can write HTML content JavaScript"
+document.write(work);
+// question # 09
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design);
