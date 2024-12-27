321d950a3921c77460b3ac99d5418e412cbd555d
Marwa-arsal <marwa.arsal@hotmail.com>
2024-12-27 10:50:45 -0800

    assignment2

---
+++ topic2/app.js
@@ -0,0 +1,60 @@
+// chapter # 3
+// question # 01
+var age = "20";
+alert("i am 20 years old");
+// question # 02
+var visited = "A"
+alert("you have visited this site 1 times")
+var visited = "B"
+alert("you have visited this site 2 times")
+var visited = "C"
+alert("you have visited this site 3 times")
+var visited = "D"
+alert("you have visited this site 4 times")
+var visited = "E"
+alert("you have visited this site 5 times")
+// question # 02
+var birthYear = "2004";
+var message = "My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof birthYear;
+document.write(message);
+// question # 03
+var VisitorsName = "John Doe"
+var ProductTitle = "T-shirt(s)"
+var Quantity = "5"
+var work = VisitorsName + " ordered " + Quantity + ProductTitle + "on XYZ Clothing store";
+document.write(work);
+// chapter # 4
+// question # 01
+// Declare 3 variables in one statement.
+var fatherName , work , surNamen;
+// question # 02
+// Declare 5 legal & 5 illegal variable names.
+  // Legal variable names
+  var fatherName ;
+  var $price ;
+  var top1 ;
+  var lastName ;
+  var _load ;
+// Illegal variable names 
+// var Father name ;
+// var father-Name ;
+// var 2work ;
+// var stop! ;
+// var var ; 
+// question # 03
+// A heading stating “Rules for naming JS variables”
+// var main = " <h1> Rules for naming JS variables </h1> "
+// document.write(main);
+// Variable names can only contain ______, ______,
+// ______ and ______.
+// var showBox1 = "<p>$me , 1only , fatherName</p>"
+// document.write(showBox1)
+// Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// var showBox2 = "<p>1top, _work , name</p>"
+// document.write(showBox2)
+// d) Variable names are case _________
+{/* <p>sensitive</p> */}
+// Variable names should not be JS _________
+{/* <p>keywords</p> */}
+
